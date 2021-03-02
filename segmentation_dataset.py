643cb4e8cb7159318654266113a990bcae31561d
Gene Tanaka <genetanaka@Genes-MBP.attlocal.net>
2021-03-01 22:33:25 -0800

    added segmentation_dataset.py for pre-processing Wiki data

---
+++ segmentation_dataset.py
@@ -0,0 +1,42 @@
+from pathlib2 import Path
+from torch.utils.data import Dataset
+
+class SegmentationDataset(Dataset):
+    def __init__(self, root_dir: str):
+        PAD_STR = '<pad>'
+        BOUNDARY_STR = '<boundary>'
+        self.examples = []
+        self.targets = []
+        all_objects = Path(root_dir).glob('**/*')
+        self.filenames = [str(p) for p in all_objects if p.is_file() and str(p).split("/")[-1] != '.DS_Store']
+        passages = []
+        for filename in self.filenames:
+            file = open(str(filename), "rt", encoding="utf8")
+            raw_content = file.read()
+            file.close()
+            clean_txt = raw_content.strip()
+            sentences = [s for s in clean_txt.split("\n") if len(s) > 0 and s != "\n"]
+            passages.append(sentences)
+        max_len = max([len(s) for s in passages])
+        for passage in passages:
+            target = [0]
+            for i in range(1,len(passage)):
+                if passage[i - 1][:3] == '===':
+                    target[-1] = 1
+                    passage[i - 1] = BOUNDARY_STR
+                else:
+                    target.append(0)
+            target += [0]*(max_len - len(target))
+            self.targets.append(target)
+            example = [word for word in passage if word != BOUNDARY_STR]
+            example += [PAD_STR]*(max_len - len(example))
+            self.examples.append(example)
+    
+    def __len__(self):
+        return len(self.examples)
+    
+    def __getitem__(self, idx):
+        ret_vals = {}
+        ret_vals['sentences'] = self.examples[idx]
+        ret_vals['target'] = self.targets[idx]
+        return ret_vals
